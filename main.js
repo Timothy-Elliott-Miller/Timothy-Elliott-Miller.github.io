6598694744782082ff8fd472d7f5eb046f3167f6
Tim <Timothy.Elliott.Miller@gmail.com>
2020-10-06 15:21:09 +1100

    6th commit

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 var button = document.getElementById("button1");
 var display = document.getElementById("display1");
 var count = 0;
-console.log("js loaded")
+
 
 button.onclick = function() {button1Clicked()};
 
 function button1Clicked(){
   console.log("button1 clicked")
+  count ++;
   display.text = count;
 }
