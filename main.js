b61c8b724896765dc8ca9a8bdda30577ca010db0
Tim <Timothy.Elliott.Miller@gmail.com>
2020-10-06 15:17:24 +1100

    3rd commit

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 var button = document.getElementById("button1");
 var display = document.getElementById("display1");
+var count = 0;
+
+button.onclick = function() {button1Clicked()};
+
+function button1Clicked(){
+  display.text = count;
+}
